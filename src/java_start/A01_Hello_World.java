71d17b31353a80c8a49ac1eed127949c92972344
Abhishek <shubham.abhi@gmail.com>
2024-02-28 17:50:25 +0530

    hello world is updated and demo.java is added

--- src/java_start/A01_Hello_World.java
+++ src/java_start/A01_Hello_World.java
@@ -2,6 +2,8 @@ package java_start;
 
 public class A01_Hello_World {
 	
+	//comment added to check git reaction
+	
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
